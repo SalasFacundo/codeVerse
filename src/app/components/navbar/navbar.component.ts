38951d498b32808354ec379f6556259f9d1da3f2
Facundo <salasfacundo96@gmail.com>
2023-04-08 12:20:11 -0300

    implementacaion de sidenav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,11 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isOpened: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  
-
 }

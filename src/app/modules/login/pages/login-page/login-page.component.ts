246f4968d61341e84ed89d1ac7979c8d21b9e6f8
Facundo <salasfacundo96@gmail.com>
2023-04-13 17:44:06 -0300

    Se agregaron los usuarios  para probar

--- src/app/modules/login/pages/login-page/login-page.component.ts
+++ src/app/modules/login/pages/login-page/login-page.component.ts
@@ -10,6 +10,8 @@ export class LoginPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    alert("Usuarios para probar en la consola del navegador");
+    console.log("Usuarios de prueba\n\n Admin: \n User: admin@admin \n Password: admin \n\n NO admin: \n User: ana.garcia@example.com \n Password: password")
   }
 
 }

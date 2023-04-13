f851f11853efce112098a8c3b5ecde295ad34e1c
Facundo <salasfacundo96@gmail.com>
2023-04-13 16:36:43 -0300

    Se valida el login, y guarda el usuario loggeado en un servicio

---
+++ src/app/modules/students/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {    
+    id: number;
+    dni: number;
+    name: string;
+    lastName: string;
+    email: string;
+    password: string;
+    isAdmin: boolean;
+    
+}
---
+++ src/app/services/user-logged.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { User } from '../modules/students/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserLoggedService {
+
+  user: User = {
+    id: 1,
+    dni: 1,
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    isAdmin: false
+  };
+
+  constructor() { }
+
+  setUser(user: User): void {
+    this.user = user; 
+  }
+
+  getUser(): User {
+    return this.user;
+  }
+
+}

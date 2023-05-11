a06f91e541dd471bc40e51fdabbf43dd97769747
Facundo <salasfacundo96@gmail.com>
2023-05-10 22:51:21 -0300

    Se creo unit test para componente default navbar

--- src/app/feature_modules/navbar/components/default-navbar/default-navbar.component.ts
+++ src/app/feature_modules/navbar/components/default-navbar/default-navbar.component.ts
@@ -6,22 +6,26 @@ import { UserLoggedService } from 'src/app/services/user-logged.service';
 @Component({
   selector: 'app-default-navbar',
   templateUrl: './default-navbar.component.html',
-  styleUrls: ['./default-navbar.component.scss']
+  styleUrls: ['./default-navbar.component.scss'],
 })
 export class DefaultNavbarComponent implements OnInit {
-
   loggedUser!: User;
   grillaSize: number = 0;
+  welcomeMessage!: string;
 
-  constructor(private datosService: DatosService,
-              private user: UserLoggedService) { }
+  constructor(
+    private datosService: DatosService,
+    private user: UserLoggedService
+  ) {}
 
   ngOnInit(): void {
     this.loggedUser = this.user.getUser();
+    this.welcomeMessage = this.loggedUser.isAdmin
+      ? 'Bienvenido al área administrador, aqui podrás agregar, modificar, o eliminar tanto cursos, como alumnos, y visualizar sus datos!'
+      : '¡Aqui podrás visualizar tus cursos!';
   }
 
-  onGrillaSize(event: any){
+  onGrillaSize(event: any) {
     this.grillaSize = event;
   }
-
 }

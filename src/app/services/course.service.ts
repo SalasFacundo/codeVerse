09279146b00f5bfee5783f5141aa62dc4780839a
Facundo <salasfacundo96@gmail.com>
2023-06-20 22:17:46 -0300

    Se agrego servicio para Course

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Course } from '../models/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  courses: Course[] = [];
+  private coursesSubject = new BehaviorSubject<Course[]>([]);
+  urlEndpoint: string = 'http://localhost:8080/api/courses';
+  body = `{
+    "name": "Curso 11112226",
+              "description": "Descripción del curso 1",
+              "price": 99,
+              "capacity": 20,
+              "teacherId": 2,
+              "classesId": "1, 2, 3",
+              "startDate": "2023-06-09T23:00:00.000-04:00",
+              "endDate": "2023-07-09T23:00:00.000-04:00",
+              "startHour": "09:00:00",
+              "endHour": "12:00:00"
+
+  }`;
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor( private httpClient: HttpClient) {
+    this.loadCourses();
+  }
+
+  loadCourses(){
+    this.httpClient.get<any>(this.urlEndpoint+'/all').subscribe(response => {
+      this.coursesSubject.next(response.courses);
+    });
+  }
+
+  getAllCourses(){
+    return this.coursesSubject.asObservable();
+  }
+
+  modifyCourse(id: number, course: Course){
+
+    this.httpClient.put<any>(this.urlEndpoint+'/update/'+id, course, {headers: this.headers}).subscribe(response => {
+      console.log("RESPONSE");console.log(response);
+      this.loadCourses();
+    });
+  }
+}
